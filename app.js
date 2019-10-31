8d5a9c595b76301b2843a0a2c90a73107c7c9f8b
Julio García Pérez <julio.garciaperez@telefonica.com>
2019-10-31 18:56:17 +0100

    minor fixes

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 /* Middlewares config */
 
 app.use(express.static('public'));
-app.use(expressLayouts);
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('layout', 'layout');
+app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 /* App routes */
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('Ready!')
+});
